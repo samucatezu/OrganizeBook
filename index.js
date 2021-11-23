a607503980d888d54b842ac1ae639346ea647934
Samuel barbosa almeida <barbosasamuel24@gmail.com>
2021-11-22 22:04:26 -0300

    Fix(index.js): syntax erros

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const livros = [`Doze regras para a vida`, `Storynomics`, `Cant hurt me`]
 
 // retorna um livro
 
-server.get(`/livros/:index`, (req, res) =>{
+server.get('/livros/:index', (req, res) => {
     const { index } = req.params;
     
-    return res.json(cursos[index]);
+    return res.json(livros[index]);
 });
 
 // retornar todos os livros
@@ -43,8 +43,8 @@ server.delete('/livros/:index', (req, res) => {
     const { index } = req.params;
 
     livros.splice(index, 1);
-    return res.json({ message: "O curso foi deletado"});
-    
+    return res.json({ message: "O livro foi deletado"});
+
 })
 
 server.listen(3000);
